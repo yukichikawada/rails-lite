b4652df58cd265985b2a209ed576a07742f8cb8e
Yukichi Kawada <yukichikawada@gmail.com>
2018-03-14 16:35:56 -0700

    #render and #redirect complete

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -8,20 +8,34 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req, @res = req, res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise if @already_built_response
+
+    @res.header['location'] = url
+    @res.status = 302
+    
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise if @already_built_response
+
+    @res['Content-Type'] = content_type
+    @res.write(content)
+
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
@@ -37,4 +51,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
